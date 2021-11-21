247ea6e2f0f16e41cfcb68556e716f08a5694820
beauhilton <94798454+beauhilton@users.noreply.github.com>
2021-11-21 10:57:58 -0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,48 @@
+# neovimbook
+
+This repository has the contents of the 2021-2022 Vanderbilt Internal Medicine Handbook,
+scraped from the website, so I can have working search and experiment with different rendering engines.
+
+Below are some notes for my own use.
+
+# VA notes
+
+## tricks
+
+### Cleaning up old orders
+
+_Shout out to the BMT nurses for showing me this trick_
+
+Some folks have literally years 
+of old nursing orders and administrative mumbo jumbo 
+in their orders tab that is not only useless, 
+it is dangerous, because it makes it harder for everyone
+(nurses included) to see current, relevant orders.
+
+Cleaning out old orders is easy.
+Highlight all the orders you want 
+(CTRL-click is better than SHIFT-click, 
+bc you can drag a CTRL-click and skip some,
+then drag some more),
+right click, 
+select "Discontinue,"
+then click "Duplicate Order."
+
+After you sign these, 
+they should disappear from the chart immediately.
+
+Oddities:
+
+For some reason, 
+no orders from 2008 or earlier are able to be deleted.
+If you figure out how to fix this, let me know.
+
+Most old "pending" lab orders can be deleted,
+but "active" ones (even if super old)
+stick around like peanut butter stains.
+
+## orders of much consternation
+
+- Coombs/DAT: (not in Lab Tests...) Blood Bank Orders (VBECS) -> Blood Bank Orders -> Diagnostic Tests -> Direct Antiglobulin Test
+- topical TXA: Meds: Inpatient ->  tranexamic acid inj -> the option that says \*VL\*, also specify topical in the comments (thanks inpt pharm!)
+- procedure service: not available at the VA
---
+++ get_links.py
@@ -0,0 +1,84 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup
+import pandas as pd
+import pypandoc
+
+session = HTMLSession()
+
+
+def get_top_links(url: str):
+
+    r = session.get(url)
+    links = [link for link in r.html.absolute_links if "commodorecompendium" in link]
+
+    return links
+
+
+def get_subject_links(url: str):
+    r = session.get(url)
+    subjects = r.html.find("#content", first=True)
+    links = [link for link in subjects.absolute_links if "commodorecompendium" in link]
+    return links
+
+
+def get_html(url: str):
+    r = session.get(url)
+    content = r.html.find("#content", first=True)
+    return content.html
+
+
+def convert_table(table):
+    df = pd.read_html(str(table))[0]
+    table.string = df.to_html(index=False)
+    return table
+
+
+links = get_top_links("https://medsites.vumc.org/commodorecompendium/introduction")
+
+for link in links:
+    try:
+        subjects = get_subject_links(link)
+        for subject in subjects:
+            try:
+                print("\n", subject)
+                res = get_html(subject)
+                soup = BeautifulSoup(res, "lxml")
+
+                try:
+                    tables = soup.find_all("table")
+                    for table in tables:
+                        convert_table(table)
+                except Exception as exc:
+                    print(f"no table in {subject}, {exc}")
+
+                for data in soup(["script", "head"]):
+                    data.decompose()
+                soup = soup.prettify(formatter=None)
+
+                file_name = f"{link.split('/')[-1]}-{subject.split('/')[-1]}"
+                html_file = f"content/html/{file_name}.html"
+
+                md_file = f"content/md/{file_name}.md"
+                with open(html_file, "w", encoding="utf-8") as f:
+                    f.write(str(soup))
+
+                output = pypandoc.convert_file(
+                    source_file=html_file,
+                    format="html-native_divs-native_spans",
+                    to="gfm",
+                    outputfile=md_file,
+                )
+
+                # make first line a # header so bookdown will work
+                with open(md_file) as f:
+                    firstline = f.readline()
+                    lines = f.readlines()
+                lines[0] = f"# {firstline} \n"
+                with open(md_file, "w") as f:
+                    f.writelines(lines)
+
+            except Exception as exc:
+                print(f"no table in {subject}, {exc}")
+
+    except Exception as exc:
+        print(exc)
