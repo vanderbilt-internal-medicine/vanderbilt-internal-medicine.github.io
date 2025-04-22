e5fa552d9bdf56a53b42069fb8a855390f109e72
sameersundrani <48189633+sameersundrani@users.noreply.github.com>
2025-04-21 21:01:08 -0500

    Changed chatbot config to entirely cloudbased

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -19,8 +19,10 @@ document$.subscribe(() => {
             return userId;
         };
         Chatbot.init({
-            chatflowid: "1887fe55-12c2-448b-8a3e-4a0a57a7841f",
-            apiHost: "https://flowise-public.vlr.chat",
+            // chatflowid: "1887fe55-12c2-448b-8a3e-4a0a57a7841f",
+            // apiHost: "https://flowise-public.vlr.chat",
+            chatflowid: "93708edc-bfb7-4fee-acfc-8f6a0fa72e02",
+            apiHost: "https://xlr-chat.app.flowiseai.com",
             chatflowConfig: {
                 // topK: 2
                 // userId: getUserId(), // Add the userId here
